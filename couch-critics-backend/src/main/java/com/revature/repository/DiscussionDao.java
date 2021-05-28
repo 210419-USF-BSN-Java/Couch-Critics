078127dae95efc60dc7d93b7df7962b6c4c5fb36
Kemo Allen <kemo@claudia-allens-iMac-5.local>
2021-05-27 19:07:46 -0500

    made service folder

--- couch-critics-backend/src/main/java/com/revature/repository/DiscussionDao.java
+++ couch-critics-backend/src/main/java/com/revature/repository/DiscussionDao.java
@@ -2,8 +2,11 @@ package com.revature.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import com.revature.model.Discussion;
 
+@Repository
 public interface DiscussionDao {
 	
 	public List<Discussion> getDiscussionByUserId(int userID);
