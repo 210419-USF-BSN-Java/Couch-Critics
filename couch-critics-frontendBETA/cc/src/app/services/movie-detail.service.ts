1e8cb4fce3eca04b6d2908b8612dd244ab2c6e51
Steven <nguyens612@hotmail.com>
2021-06-04 18:47:40 -0500

    movie interface added

---
+++ couch-critics-frontendBETA/cc/src/app/services/movie-detail.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {Movie} from '../models/movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieDetailService {
+
+  constructor() { }
+}
