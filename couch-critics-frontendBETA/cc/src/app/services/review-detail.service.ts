a389c6e48043382b1c949fb3ac9cbf14fd559787
Steven <nguyens612@hotmail.com>
2021-06-04 16:49:38 -0500

    made changes to review model

--- couch-critics-frontendBETA/cc/src/app/services/review-detail.service.ts
+++ couch-critics-frontendBETA/cc/src/app/services/review-detail.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import {review} from '../models/review-interface';
 import {REVIEWS} from '../critics-stories/reviews';
 import {Observable, of} from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http'; 
+import { environment as env} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReviewDetailService {
 
-  private reviewUrl = '/review';
+  private reviewUrl = env.BACKEND_URL;
+  private testUrl = "https://api.themoviedb.org/3/movie/550?api_key=dd454d7a4e6f0047a483673b42f31898";
 
   constructor(private http: HttpClient) { }
 
@@ -26,18 +28,26 @@ export class ReviewDetailService {
 
   /// functions below are attempting to retrieve observables from our server
   /// Error returning is skipped for now
-  getR():Observable<review[]>{
-    return this.http.get<review[]>(this.reviewUrl)
+
+  //Get all reviews
+  getR(id:number):Observable<review[]>{
+    let url = `${this.reviewUrl}/viewByCriticId/${id}`;
+    return this.http.get<review[]>(url);
   }
 
+  //get all pending
   getPendingStatus(p:String){
     let url = `${this.reviewUrl}/viewByReviewStatus/{p}`;
     return this.http.get<review>(url);
   }
 
+  //get all accepted
   getAcceptedStatus(a:String){
-    let url = `${this.reviewUrl}/viewByReviewStatus/{a}`;
+    let url = `${this.reviewUrl}/viewByReviewStatus/accepted`;
     return this.http.get<review>(url);
   }
 
+  getTest():Observable<Object>{
+    return this.http.get('https://api.themoviedb.org/3/movie/550?api_key=dd454d7a4e6f0047a483673b42f31898');
+  }
 }
