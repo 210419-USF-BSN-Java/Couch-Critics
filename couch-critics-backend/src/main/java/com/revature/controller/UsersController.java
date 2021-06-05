fc3bb54a214e2396ef23c733afb673973f5680cf
Ruben <rubengarcia0515@gmail.com>
2021-06-05 16:04:47 -0700

    login componenet validation progress

--- couch-critics-backend/src/main/java/com/revature/controller/UsersController.java
+++ couch-critics-backend/src/main/java/com/revature/controller/UsersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -88,7 +89,7 @@ public class UsersController {
 		return new ResponseEntity<List<Users>>(u, HttpStatus.OK);
 	}
 	
-	@PostMapping(value="/getUsersById/{id}")
+	@GetMapping(value="/getUsersById/{id}")
 	public ResponseEntity<Users> getUsersByRole(@PathVariable int id){
 		Users u = us.getUserById(id);
 	
--- couch-critics-frontendBETA/cc/src/app/login/login.component.ts
+++ couch-critics-frontendBETA/cc/src/app/login/login.component.ts
@@ -12,6 +12,7 @@ export class LoginComponent implements OnInit {
 
   username: string = '';
   password: string = '';
+  accountActivated: Boolean | undefined; 
 
   constructor(private authServ : AuthenticationService, private router: Router) { }
 
@@ -23,29 +24,37 @@ export class LoginComponent implements OnInit {
     this.authServ.login(this.username, this.password).subscribe(
       response =>{
         console.log(response)
-        let userId = response.userid; 
-        //this works, sets as json format in sessionstorage
+        if((response.userid !== null)) {
+        console.log("successful login, user is activated and credentials matched")
+        let userId = response.userid;
+        if(response.statusId?.accStatusId === 1){
+             //this works, sets as json format in sessionstorage
         //session storage gets deleted automatically when exiting specific tab initially logged in,closing browser
+        this.accountActivated = false; 
         window.sessionStorage.setItem('currentUserid', JSON.stringify(userId));
         window.sessionStorage.setItem('currentUserObject', JSON.stringify(response));
-
         let userType = response.roleId?.roleId;
+          switch(userType) {
+            case 1:
+              this.router.navigate(['admin']);
+              break;
 
-        switch(userType) {
-          case 1:
-            this.router.navigate(['admin']);
-            break;
+            case 2:
+              this.router.navigate(['critics']);
+              break;
+        
+            case 3:
+              this.router.navigate(['userhome']);
+              break;
+            default:
+              alert("Something went wrong!");
+              this.router.navigate(['']);
+        } 
+          }else if(response.statusId?.accStatusId === 2){
+            this.accountActivated= false
+          }
+        }else if(response.userid === null){
 
-          case 2:
-             this.router.navigate(['critics']);
-             break;
-      
-          case 3:
-            this.router.navigate(['userhome']);
-            break;
-          default:
-            alert("Something went wrong!");
-            this.router.navigate(['']);
         }
        
       },
