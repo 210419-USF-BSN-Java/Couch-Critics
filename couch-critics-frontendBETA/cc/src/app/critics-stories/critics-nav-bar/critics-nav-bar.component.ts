38de965c81e2018066a413c10aac940613896c37
Steven <nguyens612@hotmail.com>
2021-06-06 21:37:43 -0500

    added critics navbar componenet

---
+++ couch-critics-frontendBETA/cc/src/app/critics-stories/critics-nav-bar/critics-nav-bar.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-critics-nav-bar',
+  templateUrl: './critics-nav-bar.component.html',
+  styleUrls: ['./critics-nav-bar.component.css']
+})
+export class CriticsNavBarComponent implements OnInit {
+
+  constructor(private router : Router) { }
+
+  ngOnInit(): void {
+  }
+
+
+  logout() {
+    sessionStorage.clear();
+    this.router.navigate(['']);
+    alert("you are logged off");
+  }
+
+  home(){
+    this.router.navigate(['/critics'])
+  }
+
+  makeReview(){
+    this.router.navigate(['/create'])
+  }
+}
