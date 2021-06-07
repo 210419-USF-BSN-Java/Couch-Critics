e4060b18a4e369bf69daee6b8dd8e80e3becbbdc
Ruben <rubengarcia0515@gmail.com>
2021-06-06 20:16:53 -0700

    added service methods for discussions

---
+++ couch-critics-frontendBETA/cc/src/app/services/discussion.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { environment as env} from '../../environments/environment';
+import {HttpClient, HttpHeaders} from "@angular/common/http"
+import {discussion} from "../models/discussion"
+
+import {Observable, } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DiscussionService {
+  
+    private baseUrl = env.BACKEND_URL;
+
+  constructor(private http: HttpClient, private HttpClient: HttpHeaders) { }
+
+  getCommentsForReview(reviewId: number): any{
+    const url = `${this.baseUrl}/disc/reviews/${reviewId}`
+    return this.http.get<any>(url); 
+  }
+
+  addCommentForReview(disc: discussion ){
+    const url = `${this.baseUrl}/disc/add`
+    return this.http.post<discussion>(url, disc,httpOptions)
+    //this endpoint from server returns a boolean based on status 
+
+  }
+
+}
+
